fb8e6b7e1907814c6dc5283b7b998f2d6c85d682
ThaeThae <soethiha@innov8te.com.sg>
2018-11-03 23:21:14 +0630

    add search book, view more button and rotate image gallary

--- 3. ajax-sample/js/app.js
+++ 3. ajax-sample/js/app.js
@@ -1,24 +1,43 @@
 
 const model = {
     apiUrl : 'https://www.googleapis.com/books/v1/volumes', 
-    keywork : 'javascript',
+    keyword : 'javascript',
     currentBook: {},
-    books: []
+    books: [],
+    startIndex : 0,
+    totalAvailableBooks : 0,
+    hasMoreBook : true,
+    itemPerPage : 10,
+    nextStartIndex : function(){
+        return model.startIndex + model.itemPerPage;
+    }
+
 }
 
 const controller = {
     init: function () {
         this.retrieveBookFromAPI();
         bookView.init();
+        bookListView.init();
+        bookSearchView.init();
     },
-    retrieveBookFromAPI: function (startIndex = 0) {
-        fetch(`${model.apiUrl}?q=${model.keywork}&startIndex=${startIndex}`)
+    retrieveBookFromAPI: function () {
+        // console.log(model.startIndex);
+        fetch(`${model.apiUrl}?q=${model.keyword}&startIndex=${model.startIndex}`)
             .then(function (response) {
                 return response.json();
             })
-            .then(function (books) {
-                model.books = books.items;
+            .then(function (books) {                
+                model.books = model.books.concat(books.items);
                 model.currentBook = books.items[0];
+
+                if(books.totalItems < model.nextStartIndex()) {
+                    model.hasMoreBook = false;
+                }
+                else {
+                    model.startIndex = model.nextStartIndex();
+                }
+
                 bookListView.render();
                 bookView.render();
             });
@@ -26,61 +45,99 @@ const controller = {
     getBooks: function () {
         return model.books;
     },
-
+    hasMoreBook : function(){
+        return model.hasMoreBook;
+    },
     getCurrentBook: function () {
         return model.currentBook;
+    },
+    setCurrentBook: function (book) {
+        model.currentBook = book;
+        bookView.render();
+    },
+    searchBook: function(bookname){
+        model.keyword = bookname;
+        model.startIndex = 0;
+        this.clearBookList();
+        this.retrieveBookFromAPI();
+    },
+    clearBookList: function(){
+        model.books = [];
+        bookListView.clear();
+    },
+    loadMore: function(){
+        if(model.hasMoreBook) {
+            this.retrieveBookFromAPI();
+        }
+        else {
+            alert('no more book to load');
+        }
     }
 }
 
 const bookListView = {
     init: function () {
-        this.render();
+        this.bookListElem = document.getElementById('bookList');
+
+        const viewmore = document.getElementById('btnViewMore');
+        viewmore.addEventListener('click', function(){
+            controller.loadMore();
+        })
     },
     render: function () {
-        this.books = controller.getBooks();
-        let bookListContent = '';
-        this.books.forEach(function(book,index){
-            const bookHolder = `<div class="book" data-id=${book.id} onClick="bookListView.build('${book.id}')">
-                <h3>${book.volumeInfo.title}</h3>
-                <img src=${book.volumeInfo.imageLinks.smallThumbnail} alt="${book.volumeInfo.title}">
-                </div>`;
-            bookListContent += bookHolder;
-        });
-        document.querySelector('#booklist').innerHTML = bookListContent;
-        
-        // console.log(this.books);
-
-        // console.log(model.currentBook);
+        this.clear();
+        this.books = controller.getBooks();        
+        this.books.forEach(function(book){            
+            bookListView.bookListElem.appendChild(bookListView.buildBook(book));
+        });        
     },
-    setCurrentBook: function () {
-
-    },
-    build: function(bookid){
-        this.books.forEach(function(book){
-            if(book.id === bookid) {
-                model.currentBook = book;
-            }
+    buildBook : function(book){
+        const bookDiv = document.createElement('div');
+        // console.log(book);
+        console.log(book.volumeInfo.title);
+        bookDiv.classList.add('book');
+        bookDiv.innerHTML = `
+        <div class="content">
+            <img src=${book.volumeInfo.imageLinks.smallThumbnail} alt="${book.volumeInfo.title}">
+        </div>
+        <div class="title">${book.volumeInfo.title}</div>
+        `;
+        bookDiv.addEventListener('click',function(){
+            controller.setCurrentBook(book);
         });
-        bookView.render();
+        return bookDiv;
+    },
+    clear: function(){
+        this.bookListElem.innerHTML = '';
     }
-
 }
 
 const bookView = {
     init: function(){
         this.viewport = document.getElementById('viewerCanvas');
         google.books.load();
-        google.books.setOnLoadCallback(function(){
-            bookView.render();
-        })
-        this.render();
+        google.books.setOnLoadCallback(function(){                        
+            bookView.render();    
+        });
     },
     render: function () {
-        console.log(controller.getCurrentBook());
+        console.log(controller.getCurrentBook());       
         const viewer = new google.books.DefaultViewer(bookView.viewport);
-        const currentBook = controller.getCurrentBook();
+        const currentBook = controller.getCurrentBook(); 
         viewer.load(currentBook.id);
     },
+
 }
+
+const bookSearchView = {
+    init: function(){
+        const searchbtn = document.getElementById('btnSearch');
+        searchbtn.addEventListener('click', function(){
+            const searchBookName = document.getElementById('txtSearch').value;
+            controller.searchBook(searchBookName);
+        });
+    }
+}
+
 
 controller.init();
